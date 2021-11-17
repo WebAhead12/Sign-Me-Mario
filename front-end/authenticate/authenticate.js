bcf0d981daff6a24ff6b161a25f238dcf0d328b7
George Joubran <durd2001@gmail.com>
2021-11-17 11:58:19 +0200

    renamed authenticate

---
+++ front-end/authenticate/authenticate.js
@@ -0,0 +1,5 @@
+if (window.location.search == "?error=incorrect") {
+    setTimeout(() => {
+      alert("Incorrect Login Information!");
+    }, 100);
+  }
--- handlers/authenticate.js
+++ handlers/authenticate.js
@@ -1,12 +1,31 @@
+const db = require("../database/connection");
+const path = require("path");
+
+
 const showHTML = (req,res) => {
     res.sendFile(path.join(__dirname, "..", "front-end", "authenticate", "authenticate.html"));
 }
 
 const register_post = (req,res) => {
 
+    db.query("INSERT INTO users(name,username,password,email) VALUES($1,$2,$3,$4) RETURNING id, username; ",
+    [req.body.name,req.body.username,req.body.password,req.body.email])
+    .then((result) => res.next())
+    .catch(console.error(error));
 }
 
 const log_in_post = (req,res) => {
+
+    db.query("SELECT username,password FROM users WHERE username=$1",[req.body.username,req.body.password])
+    .then(results=>{
+        let data = results.rows;
+        if(data.password == req.body.password){
+            //make cookie
+            console.log("Logged in");
+        } else {
+            res.redirect("/authenticate?error=incorrect");
+        }
+    })
 
 }
 
