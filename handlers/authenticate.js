ad0c4fa9a3b3fc54fbe7f8b59a04a065f938baba
halakhamis07 <78309585+halakhamis07@users.noreply.github.com>
2021-11-17 14:37:33 +0200

    create backend

--- handlers/authenticate.js
+++ handlers/authenticate.js
@@ -9,42 +9,42 @@ const showHTML = (req, res) => {
 };
 
 const register_post = (req, res) => {
-
-    db.query("SELECT username from USERS WHERE username=$1",[req.body.username])
-    .then(result=>{
-        if(result.rows[0]){
-            res.redirect("/authenticate?message=dumbass");
-        } else {
-            db.query("INSERT INTO users(name,username,password,email) VALUES($1,$2,$3,$4) RETURNING id, username; ",
-            [req.body.name,req.body.username,req.body.password,req.body.email]).then(result=>{
-                res.redirect("/authenticate?message=success");
-            })
-        }
-    }).catch(console.error)
-
-
-    // db.query("INSERT INTO users(name,username,password,email) VALUES($1,$2,$3,$4) RETURNING id, username; ",
-    // [req.body.name,req.body.username,req.body.password,req.body.email])
-    // .then((result) => res.next())
-    // .catch((error)=>console.error(error));
-}
-
-const log_in_post = (req,res) => {
-
-    db.query("SELECT password FROM users WHERE username=$1",[req.body.username])
-    .then(results=>{
-        let data = results.rows[0];
-        console.log("DATA: " + results.rows[0].password);
-        console.log("BODY: " + req.body.password);
-        if(data.password == req.body.password){
-            //make cookie
-            console.log("Logged in");
-            res.redirect("/")
-        } else {
-            res.redirect("/authenticate?error=incorrect");
-        }
+  db.query("SELECT username from USERS WHERE username=$1", [req.body.username])
+    .then((result) => {
+      if (result.rows[0]) {
+        res.redirect("/authenticate?message=dumbass");
+      } else {
+        db.query(
+          "INSERT INTO users(name,username,password,email) VALUES($1,$2,$3,$4)",
+          [req.body.name, req.body.username, req.body.password, req.body.email]
+        ).then((result) => {
+          res.redirect("/authenticate?message=success");
+        });
+      }
     })
-}
+    .catch(console.error);
 
-module.exports = {showHTML,register_post,log_in_post}
+  // db.query("INSERT INTO users(name,username,password,email) VALUES($1,$2,$3,$4) RETURNING id, username; ",
+  // [req.body.name,req.body.username,req.body.password,req.body.email])
+  // .then((result) => res.next())
+  // .catch((error)=>console.error(error));
+};
 
+const log_in_post = (req, res) => {
+  db.query("SELECT password FROM users WHERE username=$1", [
+    req.body.username,
+  ]).then((results) => {
+    let data = results.rows[0];
+    console.log("DATA: " + results.rows[0].password);
+    console.log("BODY: " + req.body.password);
+    if (data.password == req.body.password) {
+      //make cookie
+      console.log("Logged in");
+      res.redirect("/");
+    } else {
+      res.redirect("/authenticate?error=incorrect");
+    }
+  });
+};
+
+module.exports = { showHTML, register_post, log_in_post };
--- handlers/create.js
+++ handlers/create.js
@@ -1,5 +1,20 @@
-const get = (req, res) => {};
+const db = require("../database/connection");
+const path = require("path");
 
-const post = (req, res) => {};
+const get = (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "..", "front-end", "create", "create.html")
+  );
+};
 
+const post = (req, res) => {
+  db.query(
+    "INSERT INTO petitions(title,content,goal,image_link) VALUES($1,$2,$3,$4) RETURNING id",
+    [req.body.title, req.body.content, req.body.goal, req.body.image_link]
+  )
+    .then((result) => {
+      res.redirect(`/p/${result}`);
+    })
+    .catch(console.error);
+};
 module.exports = { get, post };
