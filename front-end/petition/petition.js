ddcd07dd785892efac5cc481e3bc62586e6727b8
halakhamis07 <78309585+halakhamis07@users.noreply.github.com>
2021-11-18 10:59:12 +0200

    zaa

--- front-end/petition/petition.js
+++ front-end/petition/petition.js
@@ -2,6 +2,13 @@ const data = new URLSearchParams(window.location.href.split("?")[1]);
 const id = data.get("id");
 const petition = document.querySelector(".petition");
 const petitionId = document.getElementById("petition_id");
+const title = document.querySelector(".title");
+const owner = document.querySelector(".owner");
+const image_link = document.querySelector(".image_link");
+const content = document.querySelector(".content");
+const goal = document.querySelector(".goal");
+
+console.log(1);
 
 //fetches the petition id to get data
 fetch(`/p/${id}`)
@@ -16,11 +23,11 @@ fetch(`/p/${id}`)
     const objArray = json;
     for (let obj of objArray) {
       let child = petition.children;
-      child[0].innerHTML = obj.title;
-      child[1].innerHTML = "Owner: ".bold() + obj.name;
-      child[2].innerHTML = "Description: ".bold() + obj.content;
-      child[3].innerHTML = `<img src=${obj.image_link} width="250px" height="300px"></img>`;
-      child[4].textContent = `Goal:${obj.signed} out of ${obj.goal}`;
+      title.innerHTML = obj.title;
+      owner.innerHTML = "Owner: ".bold() + obj.name;
+      content.innerHTML = "Description: ".bold() + obj.content;
+      image_link.innerHTML = `<img src=${obj.image_link} width="250px" height="300px"></img>`;
+      goal.textContent = `Goal:${obj.signed} out of ${obj.goal}`;
     }
   })
   .catch((error) => {
