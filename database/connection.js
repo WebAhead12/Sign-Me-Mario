d681a2f009779aa3c1abcefc1d34170bb9c9cfa4
George Joubran <durd2001@gmail.com>
2021-11-18 14:29:53 +0200

    asd

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ const pg = require("pg");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const connectionString = process.env.DB;
+const connectionString = process.env.DATABASE_URL;
 
 const db = new pg.Pool({ connectionString });
 
