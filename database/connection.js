8292e2906e77657edb1046685e1f95ad3e5c79f9
George Joubran <durd2001@gmail.com>
2021-11-18 14:38:28 +0200

    asd

--- database/connection.js
+++ database/connection.js
@@ -4,6 +4,9 @@ dotenv.config();
 
 const connectionString = process.env.DATABASE_URL;
 
-const db = new pg.Pool({ connectionString });
+const db = new pg.Pool({ 
+    connectionString,
+    ssl: {rejectUnauthorized: false}
+});
 
 module.exports = db;
